68640aff0dd0ebad58399a2b4901cf19e7d12b99
Alex Balgavy <a.balgavy@gmail.com>
2017-12-08 10:52:10 +0100

    Added an option to save the recorded audio file.
    If the file was newly recorded, a dialog box is triggered where the user
    can select to save the file. Exceptions are not handled yet, need to
    handle invalid file names.

--- notescribe.m
+++ notescribe.m
@@ -1,1 +1,14 @@
-[y,Fs] = getaudio;
+[y,Fs,load] = getaudio;
+
+if load == 0
+    savefile = questdlg('Would you like to save the file?', ...
+        'Save file?', ...
+        'Yes', 'No', 'No');
+    
+    switch savefile
+        case 'Yes'
+            filename = inputdlg('What is the path?','Path');
+            audiowrite(filename{1,1},y,Fs);
+        case 'No'
+    end
+end
