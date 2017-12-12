74ee416b76d3f154758aa7fb936cc7bda5b8ce8f
Unknown <a.balgavy@gmail.com>
2017-12-12 15:58:25 +0100

    Fixed an error with the savefile prompt

--- matlab/notescribe.m
+++ matlab/notescribe.m
@@ -7,14 +7,13 @@ elseif load == 0
     savefile = questdlg('Would you like to save the file?', ...
         'Save file?', ...
         'Yes', 'No', 'No');
-    
-    if isempty(savefile)
-        disp('User cancelled.');
-        return
-    elseif savefile == 'Yes'
-        filename = inputdlg('What is the path?','Path');
-        audiowrite(filename{1,1},y,Fs);
-    end
+    switch savefile
+        case 'Yes'
+            filename = inputdlg('What is the path?','Path');
+            audiowrite(filename{1,1},y,Fs);
+        case 'No'
+            waitfor(msgbox('Not saving file','Save cancelled'));
+    end 
 end
 
 monoy = simplesig2mono(y);
